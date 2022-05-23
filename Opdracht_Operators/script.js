8928c86747ef0511fcc3168aa522b49cff47cd2f
AlexLuttmer <81565794+AlexLuttmer@users.noreply.github.com>
2022-05-23 22:23:34 +0200

    Add files via upload

---
+++ Opdracht_Operators/script.js
@@ -0,0 +1,21 @@
+// 'And' logical operator (&&)
+
+let myAge = 18
+
+if (myAge >= 18 && myAge <=40) {
+    console.log("You can come in!")
+} else {
+    console.log("Sorry, but GTFO!")
+}
+
+// 'Or' logical operator (||)
+
+let broughtDrinks = true;
+
+if (myAge >= 18 && myAge <=40 || broughtDrinks) {
+    console.log("You can come in!")
+} else {
+    console.log("Sorry, but GTFO!")
+}
+
+// https://www.w3schools.com/js/js_comparisons.asp
